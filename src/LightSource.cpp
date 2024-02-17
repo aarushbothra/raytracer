f6edbc3d667a23a33584be66e7e3f4623f0c15f8
Aarush Bothra <olympusaarush@gmail.com>
2024-02-16 21:29:28 -0600

    fixed output for attlight

--- src/LightSource.cpp
+++ src/LightSource.cpp
@@ -1,11 +1,18 @@
 #include "LightSource.h"
 
 void LightSource::print(std::string message){
+    if (attenuatable){
+        std::cout << "att";
+    }
     std::cout << message;
     for (int i=0;i<3;i++){
         std::cout << position[i] << " ";
     }
     std::cout << !directional << " ";
     std::cout << intensity << " ";
-    std::cout << "\n";
+    if (attenuatable){
+        std::cout << c1 << " " << c2 << " " << c3 << " " << std::endl;
+    } else {
+        std::cout << std::endl;
+    }
 }
